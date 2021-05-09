09bc463b4fde21a91d9159c58ce518442a26cae4
joeltokyo <63786778+joeltokyo@users.noreply.github.com>
2021-05-09 23:54:09 +1000

    install.sh: implement backing up and unlinking files

--- install.sh
+++ install.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
-# dotfiles directory
-DOT_DEFAULT_DIR="$HOME/$DOT_DIR"
-if [ -z "$DOT_DIR" ]; then
-  export DOT_DIR="$DOT_DEFAULT_DIR"
+# dotfiles directories
+DOT_DEFAULT_SRC_DIR="$HOME/.dotfiles"
+if [ -z "$DOT_SRC_DIR" ]; then
+  export DOT_SRC_DIR="$DOT_DEFAULT_SRC_DIR"
 fi
-if ! [ -d "$DOT_DIR" ]; then
-  export DOT_DIR="$DOT_DEFAULT_DIR"
-  if ! [ -d "$DOT_DIR" ]; then
-    mkdir -p "$DOT_DIR"
+if ! [ -d "$DOT_SRC_DIR" ]; then
+  export DOT_SRC_DIR="$DOT_DEFAULT_SRC_DIR"
+  if ! [ -d "$DOT_SRC_DIR" ]; then
+    mkdir -p "$DOT_SRC_DIR"
   fi
 fi
+DOT_DST_DIR="$HOME"
+DOT_BAK_DIR="$DOT_SRC_DIR/backup"
+mkdir -p $DOT_BAK_DIR
 
+# install
 for file in {".aliases",".bashrc",".exports",".functions",".gitconfig",".profile"}; do
-  ln -s "$DOT_DIR/$file" ~/$file
+  if [ -L $file ]; then
+    unlink "$DOT_DST_DIR/$file"
+    echo "removed link for $file"
+  elif [ -f $file ]; then
+    mv "$DOT_DST_DIR/$file" "$DOT_BAK_DIR/$file"
+    echo "$file has been backed up in $DOT_BAK_DIR"
+  fi
+  ln -s "$DOT_SRC_DIR/$file" "$DOT_DST_DIR/$file"
+  echo "installed $file"
 done
