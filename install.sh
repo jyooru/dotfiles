86ed94753d7746dd380552fcf3cc3b2b9d0e5ea5
joeltokyo <63786778+joeltokyo@users.noreply.github.com>
2021-05-24 17:15:02 +1000

    move code to functions

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 detect_env () {
   if [ -d "/vscode/vscode-server" ]; then
     env="devcontainer"
@@ -16,32 +17,43 @@ detect_env () {
   fi
 }
 
-detect_env
 
-# dotfiles directories
-dot_default_src_dir="$HOME/.dotfiles"
-if [ -z "$dot_src_dir" ]; then
-  dot_src_dir="$dot_default_src_dir"
-fi
-if ! [ -d "$dot_src_dir" ]; then
-  dot_src_dir="$dot_default_src_dir"
+dot_dirs () {
+  dot_default_src_dir="$HOME/.dotfiles"
+  if [ -z "$dot_src_dir" ]; then
+    dot_src_dir="$dot_default_src_dir"
+  fi
   if ! [ -d "$dot_src_dir" ]; then
-    mkdir -p "$dot_src_dir"
+    dot_src_dir="$dot_default_src_dir"
+    if ! [ -d "$dot_src_dir" ]; then
+      mkdir -p "$dot_src_dir"
+    fi
   fi
-fi
-dot_dst_dir="$HOME"
-dot_bak_dir="$dot_src_dir/backup"
-mkdir -p "$dot_bak_dir"
+  dot_dst_dir="$HOME"
+  dot_bak_dir="$dot_src_dir/backup"
+  mkdir -p "$dot_bak_dir"
+}
 
-# install
-for file in {".aliases",".bashrc",".exports",".functions",".gitconfig",".profile"}; do
-  if [ -L "$file" ]; then
-    unlink "$dot_dst_dir/$file"
-    echo "removed link for $file"
-  elif [ -f "$file" ]; then
-    mv "$dot_dst_dir/$file" "$dot_bak_dir/$file"
-    echo "$file has been backed up in $dot_bak_dir"
-  fi
-  ln -s "$dot_src_dir/$file" "$dot_dst_dir/$file"
-  echo "installed $file"
-done
+
+install () {
+  for file in {".aliases",".bashrc",".exports",".functions",".gitconfig",".profile"}; do
+    if [ -L "$file" ]; then
+      unlink "$dot_dst_dir/$file"
+      echo "removed link for $file"
+    elif [ -f "$file" ]; then
+      mv "$dot_dst_dir/$file" "$dot_bak_dir/$file"
+      echo "$file has been backed up in $dot_bak_dir"
+    fi
+    ln -s "$dot_src_dir/$file" "$dot_dst_dir/$file"
+    echo "installed $file"
+  done
+}
+
+
+main () {
+  dot_dirs
+  install
+}
+
+
+main
