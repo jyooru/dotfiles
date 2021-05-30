2644836b35e8626eb023b5e4062fc4c8846b3bb5
jyooru <63786778+jyooru@users.noreply.github.com>
2021-05-30 11:52:46 +1000

    add vscode config

---
+++ vscode/extensions.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+code --install-extension ahmadalli.vscode-nginx-conf
+code --install-extension eamodio.gitlens
+code --install-extension enkia.tokyo-night
+code --install-extension esbenp.prettier-vscode
+code --install-extension GitHub.github-vscode-theme
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension icrawl.discord-vscode
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension PKief.material-icon-theme
+code --install-extension raynigon.nginx-formatter
+code --install-extension redhat.vscode-commons
+code --install-extension redhat.vscode-xml
+code --install-extension redhat.vscode-yaml
+code --install-extension timonwong.shellcheck
+code --install-extension yzhang.markdown-all-in-one
