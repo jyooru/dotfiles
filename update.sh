2f6e03aa95ecf3a1b27b8e0b50e47f6430b8f06f
jyooru <63786778+jyooru@users.noreply.github.com>
2021-06-04 10:16:56 +1000

    update: add animations and title

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 
+. install.sh
+
+
 update_vscode () {
   file_path=vscode/extensions.sh
   rm $file_path
@@ -12,7 +15,22 @@ update_vscode () {
 }
 
 main () {
-  update_vscode
+  cat << "EOF"
+                 _       _             _     
+ _   _ _ __   __| | __ _| |_ ___   ___| |__  
+| | | | '_ \ / _` |/ _` | __/ _ \ / __| '_ \ 
+| |_| | |_) | (_| | (_| | ||  __/_\__ \ | | |
+ \__,_| .__/ \__,_|\__,_|\__\___(_)___/_| |_|
+      |_|                                    
+EOF
+  section "configuration"
+  configure_environment
+  cd "$env" || error "could not cd into $env" "1"
+  
+  section "update"
+  if [ "$env" == "laptop" ]; then
+    update_vscode & spin "updating vscode extensions"
+  fi
 }
 
 main
