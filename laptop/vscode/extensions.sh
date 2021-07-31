61108f22730cc193d5a512eb561a9d07d91f8baa
jyooru <63786778+jyooru@users.noreply.github.com>
2021-07-31 21:58:00 +1000

    feat(laptop): update vscode config

--- laptop/vscode/extensions.sh
+++ laptop/vscode/extensions.sh
@@ -5,6 +5,7 @@ code --install-extension ahmadalli.vscode-nginx-conf
 code --install-extension eamodio.gitlens
 code --install-extension enkia.tokyo-night
 code --install-extension esbenp.prettier-vscode
+code --install-extension GitHub.copilot
 code --install-extension GitHub.github-vscode-theme
 code --install-extension GitHub.remotehub
 code --install-extension GitHub.vscode-pull-request-github
@@ -14,6 +15,7 @@ code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 code --install-extension ms-toolsai.jupyter
 code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode.live-server
 code --install-extension PKief.material-icon-theme
 code --install-extension raynigon.nginx-formatter
 code --install-extension redhat.vscode-commons
