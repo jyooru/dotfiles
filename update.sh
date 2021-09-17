dacf7759494a98b989496fe4f4d43aaeabf762be
jyooru <63786778+jyooru@users.noreply.github.com>
2021-09-17 13:10:45 +1000

    feat: install vscode extensions on apply

--- update.sh
+++ update.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 
-. install.sh
-
-
 update_vscode () {
-  file_path=vscode/extensions.sh
+  file_path=run_vscode_extensions.sh
   rm $file_path
-  printf "#!/usr/bin/env bash\n\n\n" >> $file_path
+  printf "#!/bin/bash\n\n\n" >> $file_path
   for extension in $(code --list-extensions)
   do
     echo "code --install-extension $extension" >> $file_path
@@ -15,22 +12,7 @@ update_vscode () {
 }
 
 main () {
-  cat << "EOF"
-                 _       _             _     
- _   _ _ __   __| | __ _| |_ ___   ___| |__  
-| | | | '_ \ / _` |/ _` | __/ _ \ / __| '_ \ 
-| |_| | |_) | (_| | (_| | ||  __/_\__ \ | | |
- \__,_| .__/ \__,_|\__,_|\__\___(_)___/_| |_|
-      |_|                                    
-EOF
-  section "configuration"
-  configure_environment
-  cd "${env:?}" || error "could not cd into ${env:?}" "1" 
-  
-  section "update"
-  if [ "${env:?}" == "laptop" ]; then
-    update_vscode & spin "updating vscode extensions"
-  fi
+  update_vscode
 }
 
 main
