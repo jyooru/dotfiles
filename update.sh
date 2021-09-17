2b31fc5633e23bc4cc4004ed3057669d97bea503
jyooru <63786778+jyooru@users.noreply.github.com>
2021-09-17 13:48:19 +1000

    refactor: update.sh

--- update.sh
+++ update.sh
@@ -3,10 +3,8 @@
 
 update_vscode () {
   file_path=run_vscode_extensions.sh
-  rm $file_path
-  printf "#!/bin/bash\n\n\n" >> $file_path
-  for extension in $(code --list-extensions)
-  do
+  printf "#!/bin/bash\n\n\n" > $file_path
+  for extension in $(code --list-extensions); do
     echo "code --install-extension $extension" >> $file_path
   done
 }
