1cb667517e011f73de227e287eb61c770c4d2f69
Joel <joel@joel.tokyo>
2021-09-29 22:40:25 +1000

    feat: add vscode extensions script as not all extensions can be managed by home manager

---
+++ run_vscode_extensions.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+
+if command -v code >/dev/null 2>&1; then
+  code --install-extension ahmadalli.vscode-nginx-conf
+  code --install-extension eamodio.gitlens
+  code --install-extension enkia.tokyo-night
+  code --install-extension esbenp.prettier-vscode
+  code --install-extension GitHub.github-vscode-theme
+  code --install-extension GitHub.remotehub
+  code --install-extension GitHub.vscode-pull-request-github
+  code --install-extension icrawl.discord-vscode
+  code --install-extension miguelsolorio.min-theme
+  code --install-extension ms-azuretools.vscode-docker
+  code --install-extension ms-python.python
+  code --install-extension ms-python.vscode-pylance
+  code --install-extension ms-toolsai.jupyter
+  code --install-extension ms-vscode-remote.remote-containers
+  code --install-extension ms-vscode.live-server
+  code --install-extension naumovs.color-highlight
+  code --install-extension PeterStaev.lego-spikeprime-mindstorms-vscode
+  code --install-extension PKief.material-icon-theme
+  code --install-extension pranaygp.vscode-css-peek
+  code --install-extension raynigon.nginx-formatter
+  code --install-extension redhat.vscode-commons
+  code --install-extension redhat.vscode-xml
+  code --install-extension redhat.vscode-yaml
+  code --install-extension rust-lang.rust
+  code --install-extension timonwong.shellcheck
+  code --install-extension yzhang.markdown-all-in-one
+fi
+
