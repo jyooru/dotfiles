e6549b0be51590e5b4534725374c5bae6125cd8c
Joel <joel@joel.tokyo>
2021-10-15 21:52:05 +1000

    feat: setup nebula

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ./packages.nix ];
+  imports = [ ./hardware-configuration.nix ./packages.nix ./nebula.nix ];
 
   boot = {
     loader.systemd-boot.enable = true;
