a2245522992a8edc553af01d878c574010c7aaf3
Joel <joel@joel.tokyo>
2021-10-23 18:51:04 +1000

    feat: install flakes

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,12 @@
     steam.enable = true;
   };
 
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
   nixpkgs.config.allowUnfree = true;
 
   system = {
