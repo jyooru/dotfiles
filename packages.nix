bd66bae72898d61581cb755ea9eb87a3b190b56f
Joel <joel@joel.tokyo>
2021-10-23 16:52:01 +1000

    refactor: organise packages.nix

--- packages.nix
+++ packages.nix
@@ -2,80 +2,85 @@
 
 {
   environment.systemPackages = with pkgs; [
+    # apps
+    brave # browser
     firefox # browser
-    python3 # programming language
-    sshfs # mount filesystems over ssh
-    dig # dns lookup
-    htop # system usage monitor
-    git # version control system
-    brave # browser 
-    fira-code # monspace font
     vscode # code editor
-    gimp # photo editor
-    obsidian # note app
-    signal-desktop # chat app (family, friends)
-    discord # chat app (friends, public)
+    signal-desktop # chat (family, friends)
+    discord # chat (friends, public)
+    gnome.nautilus # file manager (gui)
+    ranger # file manager (tui)
     spotify # music streaming service
-    qbittorrent # torrent app
+    obsidian # notes
+    onlyoffice-bin # office suite
+    libreoffice # office suite
     bitwarden # password manager
-    starship # terminal prompt
-    killall
-    scrot # screenshotter
-    neofetch
-    cmatrix
-    bspwm # window manager
-    alacritty # terminal emulator
-    polybar # bar
-    rofi # launcher
-    wmctrl # cli tool for interacting with window manager
-    gnome.nautilus # file manager
-    lsd # pretty ls alternative
-    ranger # terminal file manager
-    pv # pipe progress monitor
-    ncdu # disk space viewer
-    ffmpeg # video converter
-    nmap # port scanner
-    iotop # I/O monitor
-    pandoc # document converter
-    picom # compositor
-    fd # find alternative
-    ffsend # firefox send cli
-    brightnessctl # control laptop screen brightness
-    xorg.xev # tool to get x key press names
-    playerctl # tool to control music playing
-    nixpkgs-fmt # nix formatter
-    darktable # raw developer
+    darktable # photo editor and raw developer
+    gimp # photo editor
     qimgv # photo viewer
-    betterlockscreen # lock screen
-    docker-compose # tool to manage multi-container applications
+    qbittorrent # torrent app
+
+    # code
+    nodePackages.prettier # * formatter
+    caddy # caddyfile formatter and web server
+    python3 # python
     python3Packages.black # python formatter
     python3Packages.flake8 # python linter
     python3Packages.isort # python import formatter
     python3Packages.mypy # python type checker
     python3Packages.poetry # python package manager
     python3Packages.pytest # python test framework
+    nixpkgs-fmt # nix formatter
+
+    # desktop environment
+    polybar # bar
+    picom # compositor
+    fira-code # font
+    rofi # launcher
+    betterlockscreen # lock screen
+    alacritty # terminal emulator
+    starship # terminal prompt
+    bspwm # window manager
+
+    # optional dependencies
+    file # for preview in ranger
+    python3Packages.chardet # for preview in ranger
+    highlight # for preview in ranger
+    unzip # for preview in ranger
+    python3Packages.pdftotext # for preview in ranger
+    mediainfo # for preview in ranger
+    odt2txt # for preview in ranger
+
+    # tools
+    brightnessctl # brightness control
+    dig # dns lookup
+    pandoc # document converter
+    ncdu # disk space viewer
     p7zip # file archiver, optional dependecy for preview in ranger
-
-    # ranger preview
-    file # optional dependecy for preview in ranger
-    python3Packages.chardet # optional dependecy for preview in ranger
-    highlight # optional dependecy for preview in ranger
-    unzip # optional dependecy for preview in ranger
-    python3Packages.pdftotext # optional dependecy for preview in ranger
-    mediainfo # optional dependecy for preview in ranger
-    odt2txt # optional dependecy for preview in ranger
-
-    nodePackages.prettier # formatter
-    onlyoffice-bin # office suite
-    libreoffice # office suite
-    lm_sensors # temperature monitor
-    caddy # caddy fmt for vscode extension
+    fd # "find" rust alternative
+    ffsend # file sender
     gh # github cli
-    whois
-    jq # cli json processor
-    tree
-    youtube-dl # until i get yt-dlp working
     httpie # http cli
-    # TODO: (on unstable) yt-dlp, btop
+    iotop # I/O monitor
+    jq # json cli
+    killall # kill processes by name
+    lsd # "ls" rust alternative
+    tree # list directories recursively
+    docker-compose # manage multi-container applications
+    cmatrix # matrix display
+    playerctl # media player control
+    ffmpeg # media converter
+    youtube-dl # media downloader # TODO: yt-dlp
+    sshfs # mount filesystems over ssh
+    pv # pipe progress monitor
+    nmap # port scanner
+    scrot # screenshot
+    lm_sensors # sensor viewer
+    neofetch # system info
+    htop # system usage monitor # TODO: btop
+    wmctrl # window manager control
+    xorg.xev # x event viewer
+    git # version control system
+    whois # whois viewer
   ];
 }
