99cd103a8b1b94c4ba91d4efb12b65c7ae3e684a
Joel <joel@joel.tokyo>
2021-11-20 23:02:47 +1000

    fix(hosts/ga-z77-d3h): multiple crypt devices unlocking at boot

--- hosts/ga-z77-d3h/default.nix
+++ hosts/ga-z77-d3h/default.nix
@@ -32,14 +32,30 @@
       x11.window-manager.bspwm.enable = false;
     };
     system.boot.loader.systemd-boot = {
-      enable = true;
-      device = "/dev/disk/by-uuid/646fc0f1-2d8a-4901-ae89-559154bfe288";
+      enable = false; # TODO: multiple devices. setup below
+      # device = "/dev/disk/by-uuid/646fc0f1-2d8a-4901-ae89-559154bfe288";
     };
     packages = {
       apps = false;
       code = false;
       desktopEnvironment = false;
       tools = true;
+    };
+  };
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    loader.efi.efiSysMountPoint = "/boot/efi";
+    initrd.luks.devices.crypt1 = {
+      device = "/dev/disk/by-uuid/646fc0f1-2d8a-4901-ae89-559154bfe288";
+      preLVM = true;
+      allowDiscards = true;
+    };
+    initrd.luks.devices.crypt2 = {
+      device = "/dev/disk/by-uuid/88c631bb-5335-4989-8cc5-09c4f38c8fa7";
+      preLVM = true;
+      allowDiscards = true;
     };
   };
 }
