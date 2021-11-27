1df4ba7d07565f4552f7f754cb909928302f66f5
Joel <joel@joel.tokyo>
2021-11-27 17:24:01 +1000

    fix: use nix 2.3 for nix-serve

--- default.nix
+++ default.nix
@@ -111,5 +111,11 @@
       };
     };
   };
+
+  nixpkgs.overlays = [
+    (_: super: {
+      nix-serve = super.nix-serve.override { nix = super.nix_2_3; }; # https://github.com/edolstra/nix-serve/issues/28
+    })
+  ];
 }
 
