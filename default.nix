ac678fa5ce2fcd634617400665268ff0a16d7997
Joel <joel@joel.tokyo>
2021-12-12 16:45:43 +1000

    feat: override games to always launch on dedicated gpu

--- default.nix
+++ default.nix
@@ -127,6 +127,23 @@
           ];
         });
       })
+    (_: super: {
+      steam = super.steam.override { extraProfile = "export DRI_PRIME=1"; };
+    })
+    (_: super: {
+      minecraft = super.minecraft.overrideAttrs (_: {
+        desktopItems = [
+          (pkgs.makeDesktopItem {
+            name = "minecraft-launcher";
+            exec = "env DRI_PRIME=1 minecraft-launcher";
+            icon = "minecraft-launcher";
+            comment = "Official launcher for Minecraft, a sandbox-building game";
+            desktopName = "Minecraft Launcher";
+            categories = "Game;";
+          })
+        ];
+      });
+    })
   ];
 }
 
