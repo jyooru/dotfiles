d811c0be049f6fe858f9b89d9471092384d4d146
Joel <joel@joel.tokyo>
2021-12-14 18:20:28 +1000

    feat: self is better than super for ./pkgs overlay

--- default.nix
+++ default.nix
@@ -116,7 +116,7 @@
   };
 
   nixpkgs.overlays = [
-    (_: pkgs: import ./pkgs { inherit pkgs; })
+    (pkgs: _: import ./pkgs { inherit pkgs; })
     (_: super: {
       nix-serve = super.nix-serve.override { nix = super.nix_2_3; }; # https://github.com/edolstra/nix-serve/issues/28
     })
