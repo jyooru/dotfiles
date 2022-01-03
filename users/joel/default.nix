88d91c0f5326279a4bd40c7e79ee80cb611b508f
Joel <joel@joel.tokyo>
2022-01-03 23:21:47 +1000

    fix: get git profile working

--- users/joel/default.nix
+++ users/joel/default.nix
@@ -1,17 +1,13 @@
-{ ... }:
+{ hmUsers, ... }:
 {
+  imports = [ ./git.nix ];
+
+  home-manager.users = {
+    inherit (hmUsers) joel;
+  };
+
   users.users.joel = {
     isNormalUser = true;
     extraGroups = [ "adbusers" "autologin" "docker" "wheel" ];
-  };
-
-  home-manager.users.joel = {
-    programs.git = {
-      enable = true;
-      signing.key = "33CA5F24";
-      signing.signByDefault = true;
-      userEmail = "joel@joel.tokyo";
-      userName = "Joel";
-    };
   };
 }
---
+++ users/joel/git.nix
@@ -0,0 +1,9 @@
+{
+  home-manager.users.joel.programs.git = {
+    enable = true;
+    signing.key = "33CA5F24";
+    signing.signByDefault = true;
+    userEmail = "joel@joel.tokyo";
+    userName = "Joel";
+  };
+}
