54a9203a128dfa698a3df5ff15459d614c70b7bf
Joel <joel@joel.tokyo>
2022-01-04 13:50:48 +1000

    refactor(users): move users out of ${user}/default.nix to ${user}.nix

--- users/joel/git.nix
+++
@@ -1,9 +0,0 @@
-{
-  home-manager.users.joel.programs.git = {
-    enable = true;
-    signing.key = "33CA5F24";
-    signing.signByDefault = true;
-    userEmail = "joel@joel.tokyo";
-    userName = "Joel";
-  };
-}
