0de1f14ba13565d8008dbd7d66b06227ab771495
Joel <joel@joel.tokyo>
2022-01-29 19:04:05 +1000

    feat(users.profiles.notification-daemon): configure timeout

--- users/profiles/notification-daemon/default.nix
+++ users/profiles/notification-daemon/default.nix
@@ -10,13 +10,14 @@
         font = "FiraCode Nerd Font 11";
         frame_width = 2;
         icon_position = "off";
+        ignore_dbusclose = true;
         offset = "24x68"; # 12x56
         show_indicators = false;
         width = "225";
       };
 
-      urgency_low.frame_color = "#303030";
-      urgency_normal.frame_color = "#444444";
+      urgency_low = { frame_color = "#303030"; timeout = 5; };
+      urgency_normal = { frame_color = "#444444"; timeout = 5; };
       urgency_critical.frame_color = "#ff0000";
     };
   };
