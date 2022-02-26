312f640dee09aa16ce96da3e83bbdbd5691fe62a
Joel <joel@joel.tokyo>
2022-02-26 12:47:31 +1000

    fix(overlays.vscode-extensions): update script has moved

--- overlays/vscode-extensions/update.fish
+++ overlays/vscode-extensions/update.fish
@@ -3,7 +3,7 @@
 set sources (readlink -f sources.sh) # full path
 set nixpkgs (nix eval --expr 'let flake = builtins.getFlake (toString ../..); in flake.inputs.nixpkgs.outPath' --impure --raw)
 
-$nixpkgs/pkgs/misc/vscode-extensions/update_installed_exts.sh $sources > tmp.nix
+$nixpkgs/pkgs/applications/editors/vscode/extensions/update_installed_exts.sh $sources > tmp.nix
 nix eval --expr '(import ./tmp.nix).extensions' --json --impure > sources.json
 
 prettier -w sources.json
