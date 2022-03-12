633b6d5d47b089dfe21ef503a41df7b48669b704
Joel <joel@joel.tokyo>
2022-03-12 12:30:52 +1000

    feat(profiles.distributed-build): only use ga-z77-d3h
    other servers aren't fast enough

--- profiles/distributed-build.nix
+++ profiles/distributed-build.nix
@@ -1,20 +1,16 @@
 { config, ... }:
 {
   nix = {
-    buildMachines = map
-      (values: {
-        systems = [ "x86_64-linux" "aarch64-linux" "armv6l-linux" ];
+    buildMachines = [
+      {
+        hostName = "ga-z77-d3h.${config.networking.domain}";
+        maxJobs = 8;
+        speedFactor = 4;
         sshUser = "joel";
-      } // values // { hostName = "${values.hostName}.${config.networking.domain}"; })
-      [
-        { maxJobs = 4; speedFactor = 1; hostName = "portege-r700-a"; }
-        { maxJobs = 4; speedFactor = 1; hostName = "portege-r700-b"; }
-        { maxJobs = 4; speedFactor = 3; hostName = "portege-z930"; }
-        { maxJobs = 8; speedFactor = 4; hostName = "ga-z77-d3h"; }
-      ];
+        systems = [ "x86_64-linux" "aarch64-linux" "armv6l-linux" ];
+      }
+    ];
     distributedBuilds = true;
-    extraOptions = ''
-      builders-use-substitutes = true
-    ''; # build machines download dependencies themselves instead of this host uploading them
+    settings.builders-use-substitutes = true;
   };
 }
