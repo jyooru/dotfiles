46b324950651af53f01ebd50e284ab205ba298d9
Joel <joel@joel.tokyo>
2022-03-13 11:23:30 +1000

    feat(profiles.server): caddy docker -> service

--- profiles/server/default.nix
+++ profiles/server/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, secrets, ... }:
 {
   # TODO: refactor
 
@@ -20,118 +20,131 @@
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "armv6l-linux" ];
 
-  networking.firewall.interfaces = {
-    "docker0".allowedTCPPorts = [ 5000 8384 ];
-    "nebula0".allowedTCPPorts = [ 80 8000 443 44300 ];
+  age.secrets."tls-joel.tokyo" = {
+    file = secrets."tls-joel.tokyo";
+    owner = "caddy";
+    group = "caddy";
   };
-  services.nginx = {
-    # :80 -> localhost:8001 (http)
-    # :8000 -> cluster:8001 (http)
-    # :443 -> localhost:8001 (https)
-    # :44300 -> cluster:44301 (https)
-    enable = true;
-    config = ''
-      worker_processes 4;
+  networking.firewall.interfaces."nebula0".allowedTCPPorts = [ 80 8000 8001 443 44300 44301 ];
+  services = {
+    nginx = {
+      # :80 -> localhost:8001
+      # :8000 -> cluster:8001
+      # :443 -> localhost:44301
+      # :44300 -> cluster:44301
+      enable = true;
+      config = ''
+        worker_processes 4;
 
-      events {
-      }
-
-      http {
-      }
-
-      stream {
-        server {
-          listen 80;
-          listen [::]:80;
-          proxy_pass localhost:8001;
+        events {
         }
 
-        upstream http_servers {
-          server 10.42.0.11:8001;
-          server 10.42.0.12:8001;
-          server 10.42.0.13:8001;
-          server 10.42.0.14:8001;
+        http {
         }
 
-        server {
-          listen 8000;
-          listen [::]:8000;
-          proxy_pass http_servers;
-        }
+        stream {
+          server {
+            listen 80;
+            listen [::]:80;
+            proxy_pass localhost:8001;
+          }
 
-        server {
-          listen 443;
-          listen [::]:443;
-          proxy_protocol on;
-          proxy_pass localhost:44301;
-        }
+          upstream http_servers {
+            server 10.42.0.11:8001;
+            server 10.42.0.12:8001;
+            server 10.42.0.13:8001;
+            server 10.42.0.14:8001;
+          }
 
-        upstream https_servers {
-          server 10.42.0.11:44301;
-          server 10.42.0.12:44301;
-          server 10.42.0.13:44301;
-          server 10.42.0.14:44301;
-        }
+          server {
+            listen 8000;
+            listen [::]:8000;
+            proxy_pass http_servers;
+          }
 
-        server {
-          listen 44300;
-          listen [::]:44300;
-          proxy_protocol on;
-          proxy_pass https_servers;
-        }
-      }
-    '';
-  };
-  home-manager.users.joel.home.file."nodeCaddyfile" = {
-    target = "node/config/Caddyfile";
-    text = let inherit (config.networking) domain fqdn hostName; in
-      ''
-        import clusterCaddyfile
+          server {
+            listen 443;
+            listen [::]:443;
+            proxy_protocol on;
+            proxy_pass localhost:44301;
+          }
 
-        tmp.${domain} {
-          import joel.tokyo
-          reverse_proxy 10.42.0.1:8080
-        }
+          upstream https_servers {
+            server 10.42.0.11:44301;
+            server 10.42.0.12:44301;
+            server 10.42.0.13:44301;
+            server 10.42.0.14:44301;
+          }
 
-        ${fqdn} {
-          import joel.tokyo
-          respond "Hello world"
+          server {
+            listen 44300;
+            listen [::]:44300;
+            proxy_protocol on;
+            proxy_pass https_servers;
+          }
         }
+      '';
+    };
+    caddy = {
+      enable = true;
+      package = pkgs.caddy-modded;
+      globalConfig = ''
+        http_port 8001
+        https_port 44301
 
-        nix.${fqdn} {
-          import joel.tokyo
-          reverse_proxy 172.17.0.1:5000
+        servers {
+          listener_wrappers {
+            proxy_protocol
+            tls
+          }
         }
+      '';
+      extraConfig =
+        let
+          inherit (config.networking) domain fqdn hostName;
+        in
+        ''
+          (joel.tokyo) {
+            import /run/agenix/tls-joel.tokyo
+          }
 
-        syncthing.${fqdn} {
-          import joel.tokyo
-          reverse_proxy ${config.services.syncthing.guiAddress}
-        }
+          joel.tokyo www.joel.tokyo {
+            import joel.tokyo
+            root * /srv/www/tokyo/joel/www
+            file_server
+            try_files {path} {path}.html {path}/
+          }
 
-        ipfs.${fqdn} {
-          import joel.tokyo
-          respond "Hello world"
-        }
-      '' + (if hostName == "portege-z930" then ''
-        vaultwarden.${domain} {
-          import joel.tokyo
-          reverse_proxy 172.17.0.1:8002
-        }
-      '' else "");
-  };
-  virtualisation.oci-containers.containers = {
-    "caddy" = {
-      image = "jyooru/caddy";
-      ports = [ "8001:80" "44301:443" ];
-      volumes = [
-        "/home/joel/node/config/Caddyfile:/etc/caddy/Caddyfile:ro" # ^
-        "/home/joel/cluster/config/Caddyfile:/etc/caddy/clusterCaddyfile:ro" # not public
-        "/home/joel/node/data/caddy:/data"
-        "/home/joel/node/log/caddy:/var/log/caddy"
-        "/home/joel/cluster/www:/srv:ro"
-      ];
+          tmp.${domain} {
+            import joel.tokyo
+            reverse_proxy 10.42.0.1:8080
+          }
+
+          ${fqdn} {
+            import joel.tokyo
+            respond "Hello world"
+          }
+
+          nix.${fqdn} {
+            import joel.tokyo
+            reverse_proxy 127.0.0.1:5000
+          }
+
+          syncthing.${fqdn} {
+            import joel.tokyo
+            reverse_proxy ${config.services.syncthing.guiAddress}
+          }
+
+          ipfs.${fqdn} {
+            import joel.tokyo
+            respond "Hello world"
+          }
+        '' + (if hostName == "portege-z930" then ''
+          vaultwarden.${domain} {
+            import joel.tokyo
+            reverse_proxy 127.0.0.1:8002
+          }
+        '' else "");
     };
   };
 }
-
-
