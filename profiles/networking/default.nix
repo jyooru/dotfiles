92eec4dcea3da8a6a180095f531c10fbcde7d3fb
Joel <joel@joel.tokyo>
2022-03-18 22:43:08 +1000

    feat(profiles.networking): forward alfis zone requests to alfis

--- profiles/networking/default.nix
+++ profiles/networking/default.nix
@@ -1,3 +1,22 @@
+with builtins;
+
+let
+  # https://github.com/Revertron/Alfis/blob/master/src/blockchain/data/zones.txt
+  alfisZones = [
+    "anon"
+    "btn"
+    "conf"
+    "index"
+    "merch"
+    "mirror"
+    "mob"
+    "screen"
+    "srv"
+    "ygg"
+  ];
+  forwardingRules = concatStringsSep "\n" (map (zone: "${zone} 127.0.0.1:5354") alfisZones);
+in
+
 {
   networking = rec {
     domain = "joel.tokyo";
@@ -12,6 +31,7 @@
   services.dnscrypt-proxy2 = {
     enable = true;
     settings = {
+      forwarding_rules = toFile "forwarding-rules.txt" forwardingRules;
       listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
       server_names = [ "cloudflare" "cloudflare-ipv6" ];
     };
