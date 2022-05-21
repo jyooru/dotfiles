799000a206d520fd8e0183db71f371252c0089b0
Joel <joel@joel.tokyo>
2022-05-21 10:49:32 +1000

    feat(profiles.ipfs): open ports for ygg0
    didn't think that it would also peer over yggdrasil, this is so cool

--- profiles/ipfs/default.nix
+++ profiles/ipfs/default.nix
@@ -1,20 +1,29 @@
 { config, pkgs, ... }:
+
+let
+  port = (import ./ports.nix).${config.networking.hostName};
+  port' = toString port;
+in
+
 {
   environment.variables.IPFS_PATH = config.services.ipfs.dataDir;
+
+  networking.firewall.interfaces."ygg0" = {
+    allowedTCPPorts = [ port ];
+    allowedUDPPorts = [ port ];
+  };
 
   services.ipfs = {
     enable = true;
     enableGC = true;
     emptyRepo = true; # no help files
     localDiscovery = true;
-    swarmAddress =
-      let port = toString (import ./ports.nix).${config.networking.hostName}; in
-      [
-        "/ip4/0.0.0.0/tcp/${port}"
-        "/ip6/::/tcp/${port}"
-        "/ip4/0.0.0.0/udp/${port}/quic"
-        "/ip6/::/udp/${port}/quic"
-      ];
+    swarmAddress = [
+      "/ip4/0.0.0.0/tcp/${port'}"
+      "/ip6/::/tcp/${port'}"
+      "/ip4/0.0.0.0/udp/${port'}/quic"
+      "/ip6/::/udp/${port'}/quic"
+    ];
   };
 
   systemd.services.ipfs.serviceConfig.ExecStartPost = "${pkgs.coreutils}/bin/chmod g+r /var/lib/ipfs/config";
