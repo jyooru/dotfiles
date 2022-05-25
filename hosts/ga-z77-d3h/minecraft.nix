c986303d2801243a6062d657e3b25c39e130aea9
Joel <joel@joel.tokyo>
2022-05-25 13:12:39 +1000

    feat(hosts.ga-z77-d3h): use minecraft server for bot testing

--- hosts/ga-z77-d3h/minecraft.nix
+++ hosts/ga-z77-d3h/minecraft.nix
@@ -1,11 +1,24 @@
 { pkgs, ... }:
 {
-  networking.firewall.interfaces."enp4s0".allowedTCPPorts = [ 25565 ];
+  networking.firewall.extraCommands =
+    let
+      command = "ip6tables -A nixos-fw -p tcp --dport 25565 -j nixos-fw-accept -i ygg0";
+    in
+    ''
+      ${command} --src 202:2e27:ab34:3bce:b238:2b1f:4c36:3780
+      ${command} --src 201:a31e:699c:23e4:8088:719f:ae26:61a7
+    '';
+
   services.minecraft-server = {
     enable = true;
-    declarative = true;
+
     eula = true;
     package = pkgs.minecraftServers.purpur_1_18_2;
-    serverProperties.motd = "\\u00A7b                    \\u00A7lplay.joel.tokyo\\u00A7r\\n\\u00A7c                          [1.18.2]";
+
+    declarative = true;
+    serverProperties = {
+      motd = "\\u00A7fSelect all squares with \\u00A7lstreet signs\\n\\u00A7rIf there are none, click skip";
+      online-mode = false;
+    };
   };
 }
