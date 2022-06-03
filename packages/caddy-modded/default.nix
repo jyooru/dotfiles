c0a329da3c32935a166a8cd576c4745757ab8506
Joel <joel@joel.tokyo>
2022-06-04 08:57:58 +1000

    chore(packages.caddy-modded): 2.4.6 -> 2.5.1

--- packages/caddy-modded/default.nix
+++ packages/caddy-modded/default.nix
@@ -1,11 +1,11 @@
 { lib, buildGoModule, ... }:
 buildGoModule {
   pname = "caddy";
-  version = "2.4.6";
+  version = "2.5.1";
 
   src = ./.;
 
-  vendorSha256 = "0kjw5lrmb052v1kryk6s3vqbv48n75i45488j35gbjya90qm5wyw";
+  vendorSha256 = "sha256-WqxPA1Ro+3ktK+yWGI8aliDy7E3ZVWnH8lyaerErckw=";
 
   meta = with lib; {
     description = "Caddy built with caddy.listeners.proxy_protocol and dns.providers.cloudflare.";
