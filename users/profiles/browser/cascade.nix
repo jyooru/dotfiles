e2de0697312aa21603004a101730c0b5e1a4153e
Joel <joel@joel.tokyo>
2022-06-04 09:16:48 +1000

    feat(users.profiles.browser): use new sri hash format to fetch cascade

--- users/profiles/browser/cascade.nix
+++ users/profiles/browser/cascade.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
     owner = "andreasgrafen";
     repo = name;
     rev = "45d558abbf856b2f198898278af1be4992e3baa2";
-    sha256 = "0i6xr1hfh3f76w1kkv0r7g45r0x6m0yi63wbx3mj4401m5pybnzc";
+    hash = "sha256-7Nvlb6kBECLr6IsPEz2opoNcyDsZ7DkDN8cN6GDI3UQ=";
   };
 
   buildPhase = ''
