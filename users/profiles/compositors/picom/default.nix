1d6367d7f98ccfa74ca1a81f8fb5418686d70389
Joel <joel@joel.tokyo>
2022-07-16 12:25:45 +1000

    fix: picom module changes

--- users/profiles/compositors/picom/default.nix
+++ users/profiles/compositors/picom/default.nix
@@ -2,8 +2,6 @@
   services.picom = {
     enable = true;
     fade = true;
-    noDNDShadow = false;
-    noDockShadow = false;
     fadeSteps = [ "0.15" "0.15" ];
     shadow = true;
     shadowOpacity = "0.25";
